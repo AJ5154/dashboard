8b00054e520161066a66f8bb0cc713412f1a66fe
AJ5154 <ashishjadhav597@gmail.com>
2023-11-26 12:13:59 +0530

    signin component v2

--- src/Components/SignIn.tsx
+++ src/Components/SignIn.tsx
@@ -1,41 +1,84 @@
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { LockOutlined } from "@mui/icons-material";
 import {
   Avatar,
+  Box,
   Button,
   Checkbox,
   Container,
+  FormControlLabel,
+  Grid,
   Link,
-  Stack,
   TextField,
   Typography,
+  TypographyProps,
 } from "@mui/material";
-import { purple } from "@mui/material/colors";
+
+const CopyRights = (props: TypographyProps) => {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center" {...props}>
+      &copy; {new Date().getFullYear()} Your Company Name. All rights reserved.
+    </Typography>
+  );
+};
 
 const SignIn = () => {
   return (
-    <>
-      <Container sx={{width:"60%"}} >
-        <Stack spacing={2}>
-          <Avatar sx={{ bgcolor: purple[500] }}>
-            <LockOutlinedIcon sx={{ color: "white" }} />
-          </Avatar>
-          <Typography variant="h5" >Sign in</Typography>
-          <TextField label="Email Address" sx={{width:"60%"}}/>
-          <TextField label="Password" sx={{width:"60%"}} />
-          <span>
-            <Checkbox />
-            Remember me
-          </span>
-          <Button type="submit" variant="contained" sx={{width:"60%"}}>
-            Submit
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 10,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlined />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            fullWidth
+            required
+            autoFocus
+            label="Email Address"
+            autoComplete="email"
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            required
+            type="password"
+            label="Password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox color="primary" />}
+            label="Remember me"
+          />
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 2 }}>
+            Sign In
           </Button>
-          <span>
-            <Link href="/forgotPassword">Forgot Password</Link>
-            <Link href="/signup">Don't have an account? Sign Up</Link>
-          </span>
-        </Stack>
-      </Container>
-    </>
+
+          <Grid container sx={{ mt: 1.5 }}>
+            <Grid item xs>
+              <Link href="/forgotPassword?" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/signin" variant="body2">
+                Don't have an account? Sign Up
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+      <CopyRights sx={{ mt: 4 }} />
+    </Container>
   );
 };
 
